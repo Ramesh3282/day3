1e54dbb8aac9c8cee06be29241351b01deb70829
Ramesh3282 <ramesharthanari.a@gmail.com>
2023-01-29 22:15:25 +0530

    Create resume.js

---
+++ resume.js
@@ -0,0 +1,23 @@
+const resume_obj={
+    name:"Ramesh",
+    email:'ramesh@gmail.com',
+    mobile:'9876543210',
+    address:'Chennai',
+    work_experince:[
+        {companyName:'company1',Numberofyears:1,designation:'Tranie software Engineer'},
+        {companyName:'company2',Numberofyears:5,designation:'software Engineer'},
+    ],
+    Skill:['Python','Javascript','REACT Js'],
+    acadamic_deatils:[
+        {degree:"B.sc",Yearofpassing:2011},
+        {degree:"MCA",Yearofpassing:2014},
+    ],
+    
+    
+    ProjectDetails:[
+        {projecttitle:'scraping1',projectdetail:'scrape the data from different source'}
+]
+
+}
+
+console.log(resume_obj)
